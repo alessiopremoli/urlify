3a288655beacc6722414c28e258105449e0c20ab
“Alessio <alessio.premoli@gmail.com>
2020-10-03 18:59:55 +0200

    gitignore

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-declare function stripLast(str: string): string;
-declare function querify(queryParams?: any): string;
-declare function pathsify(paths?: string[]): string;
-declare function urlify(url: string, paths?: string[], queryParams?: any): string;
